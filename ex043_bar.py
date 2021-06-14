2c6138fdc02f673749fc370f139a10dfb61e9d4e
Audun <audun.soberg@awacademyconsultant.no>
2021-06-14 16:43:49 +0200

    IF

--- ex043_bar.py
+++ ex043_bar.py
@@ -6,4 +6,8 @@ bar_length = 10
 num_equals = int(bar_length * current_number / total_numb)
 equals = "=" * num_equals
 spaces = " " * (bar_length - num_equals - 1)
-print(f"|{equals}>{spaces}|")
+if num_equals == bar_length:
+    print(f"|{equals}|")
+else:
+    print(f"|{equals}>{spaces}|")
+
